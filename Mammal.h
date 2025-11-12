5d9918434ff13c9b1a100ce09660f52c0c5ea975
Ali Abdulah Abouelanin <aliabdallah12376@gmail.com>
2025-11-12 15:16:52 +0200

    FOR MAMMAL.H

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,22 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-//define mammal class here
-
-
-
-
-
+class Mammal : public Animal{
+  private:
+   string furColor
+  
+  
+  public:
+   Mammal();
+   Mammal(string name, int age, bool isHungry, string furColor) : Animal(string name, int age, bool isHungry);
+   ~Mammal();
+   
+   void setfurColor();
+   string getfurColor(string furColor) const;
+};
 
 #endif
