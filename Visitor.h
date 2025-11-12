3bb7c95c0edf38c8944e9cd9b4fa3157b89180ee
Ali Abdulah Abouelanin <aliabdallah12376@gmail.com>
2025-11-12 15:19:58 +0200

    Define Visitor class with attributes and methods
    Added Visitor class with member variables and methods.

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,26 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
+class Visitor{
+  private:
+   string visitorName;
+   int ticketBought;
+   
+  public:
+   Vistor();
+   Vistor(string visitorName, int ticketBought);
+   ~vistor(); 
+   
+   void setvisitorName();
+   void setticketBought();
+   
+   string getvisitorName(string vistorName) const;
+   int getticketBought(int ticketBought) const;
+   
+   void displayInfo() const;
+};
 
 #endif
