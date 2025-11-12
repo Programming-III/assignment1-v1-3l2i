0b9ca533d2e52192ccefa72f6658018d3dbe919b
Ali Abdulah Abouelanin <aliabdallah12376@gmail.com>
2025-11-12 15:17:42 +0200

    FOR BIRD.H

--- Bird.h
+++ Bird.h
@@ -1,14 +1,20 @@
 #ifndef BIRD_H
 #define BIRD_H
-
 #include "Animal.h"
+#include <iostream>
+using namespace std;
 
-//define class bird here 
-
-
-
-
-
-
+class Bird : public Animal{
+  private:
+  float wingSpan;
+  
+  public:
+   Bird();
+   Bird(string name, int age, bool isHungry, float wingSpan) : Animal(string name, int age, bool isHungry);
+   ~Bird();
+   
+   void setwingSpan();
+   string getwingSpan(float wingSpan);
+};
 
 #endif
