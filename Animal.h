cbf9276f42064ec6edda6165336b06147ec71650
Ali Abdulah Abouelanin <aliabdallah12376@gmail.com>
2025-11-12 15:15:43 +0200

    Update Animal.h with class definition and methods
    Refactor Animal class with constructors and member functions.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,31 @@
-#ifndef ANIMAL_H
+#ifndef   ANIMAL_H
 #define ANIMAL_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal{
+  private:
+    string name;
+    int age;
+    bool isHungry;
+  
+  public:
+   Animal();
+   Animal(string name, int age, bool isHungry);
+   ~Animal();
+  
+   void setname();
+   void setage();
+   void setisHungry();
+   string getname(string name) const;
+   int getage(int age) const;
+   bool getisHungry(bool isHungry) const;
+   void display() const;
+   void feed() const;
+};
+
+#endif 
 
 
 
-#endif
