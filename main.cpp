3645acb20454a55a437e43f91e520423c5abc709
Ali Abdulah Abouelanin <aliabdallah12376@gmail.com>
2025-11-12 15:35:38 +0200

    Refactor Animal and related class implementations
    Fixed constructor and method implementations for Animal, Mammal, Bird, Reptile, Enclosure, and Visitor classes.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,90 @@
+#include <iostream>
+#include <string>
 #include "Animal.h"
-#include "Bird.h"
 #include "Mammal.h"
+#include "Bird.h"
 #include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
-#include <iostream>
 using namespace std;
+//_________________Animal______________
+
+Animal::Animal(){
+  name = ""
+  age = 0;
+  isHungry = false;
+}
+Animal::Animal(String name, int age, bool isHungry){
+  name = n;
+  age = a;
+  isHungry = h;
+}
+Animal::~Animal(){};
+
+Animal:: void display(){
+  cout << n << "(" << a << "," << h << ")" << endl;
+}
+Animal:: void feed(){
+  if 
+}
+//Mammal
+
+Mammal::Mammal(){
+  furColor =""
+}
+Mammal::Mammal(string name, int age, bool isHungry, string furColor) : Animal(string name, int age, bool isHungry){
+  furColor = fr;
+}
+Mammal::~Mammal(){};
+
+//Bird
+Bird::Bird(){
+  float wingSpan = 0.0;
+}
+Bird::Bird(string name, int age, bool isHungry, float wingSpan) : Animal(string name, int age, bool isHungry){
+  wingSpan = ws;
+}
+Bird::~Bird(){};
 
 
-// ============== MAIN FUNCTION ==============
+//Reptile
+
+Reptile::Reptile(){
+  isVenomous = false;
+}
+Reptile::Reptile(string name, int age, bool isHungry, bool isVenomous) : Animal(string name, int age, bool isHungry){
+  isVenomous = is;
+}
+Reptile::~Reptile(){};
+
+//Enclosure
+
+Enclosure::Enclosure(){
+  capacity = 0;
+  currentCount = 0;
+}
+Enclosure::Enclosure(int currentCount, int capacity, virtual [] Animal*){
+  currentCount = cc;
+  capacity = ca;
+}
+Enclosure::~Enclosure(){};
+
+//visitor
+visitor::visitor(){
+  visitorName = "";
+  ticketBought = 0;
+}
+Visitor::Visitor(string vistorName, int ticketBought){
+  vistorName = vn;
+  ticketBought = tb;
+}
+Visitor::~Visitor(){};
+
+visitor:: void displayinfo(){
+  cout << "Name: Sarah Ali"<< vn << endl;
+  cout << "Tickets Bought:" << tb << endl;
+}
+
 int main() {
     
     
